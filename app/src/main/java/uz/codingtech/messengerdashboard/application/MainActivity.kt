d44ea13beab0a79d26b2b6769e54e67bb3f5cd5b
dosto <dostondavronov67@gmail.com>
2025-12-15 17:01:25 +0500

    1

--- app/src/main/java/uz/codingtech/messengerdashboard/application/MainActivity.kt
+++ app/src/main/java/uz/codingtech/messengerdashboard/application/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.collection.orderedScatterSetOf
 import androidx.core.view.WindowCompat
 import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +16,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        userni umumiy balansi
+        orderda cancel qo'shish
+        qo'shhishda teglar kiritish
 
         setContent {
             MessengerDashboardTheme {
